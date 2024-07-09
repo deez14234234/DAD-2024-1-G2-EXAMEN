abf2a2026edfabeff15507f49ceef5b4af85e85e
deez1423 <138834774+deez1423@users.noreply.github.com>
2024-07-08 19:25:55 -0600

    asdasadsads

--- ms-pedido/src/main/java/denilson/mamani/Upeu/mspedido/dto/CategoriaDto.java
+++
@@ -1,9 +0,0 @@
-package denilson.mamani.Upeu.mspedido.dto;
-
-import lombok.Data;
-
-@Data
-public class CategoriaDto {
-    private Integer id;
-    private String nombre;
-}
--- ms-pedido/src/main/java/denilson/mamani/Upeu/mspedido/repository/PedidoRespository.java
+++
@@ -1,7 +0,0 @@
-package denilson.mamani.Upeu.mspedido.repository;
-
-import denilson.mamani.Upeu.mspedido.entity.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PedidoRespository extends JpaRepository<Pedido, Integer> {
-}
